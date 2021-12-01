694fdac0fde64b4d3989f82d2411889cbd007d38
sjaramilloo <segara968@gmail.com>
2021-12-01 13:02:40 -0500

    Se carga proyecto

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function cambiartxt1(){
+    let txt1 = document.getElementById('descripcion').textContent='A unique dark beer with a subtle pink hue in the foam. Aromas of chocolate, oak & cherry. Full flavoured and beautifully balanced.Guinness & Timmermans created this amazing blend of 3 unique beers: Guinness West Indies Porter (1801), Guinness Special Export '
+}
+
+function Cambiarimg1(){
+    let img1 = img.getElementById('producto')
+}
+
